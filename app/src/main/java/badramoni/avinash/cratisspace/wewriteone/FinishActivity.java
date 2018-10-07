3c552058ef88fcbf8b295beea8dbec8513e57fa8
AvinashBadramoni <AvinashBadramoni-avinash.badramoni@gmail.com>
2018-10-07 21:58:06 +0530

    Update app/src/main/java/badramoni/avinash/cratisspace/wewriteone/FinishActivity.java

---
+++ app/src/main/java/badramoni/avinash/cratisspace/wewriteone/FinishActivity.java
@@ -0,0 +1,15 @@
+package badramoni.avinash.cratisspace.wewriteone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.avinashbadramoni.wewriteone.R;
+
+public class FinishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish);
+    }
+}
